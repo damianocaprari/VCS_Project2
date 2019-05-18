0869f767dcb04374e569cba5e412f6f627deb1ae
damianocaprari <damiano.caprari@gmail.com>
2019-05-18 11:18:27 +0200

    ricalcolo centroide

--- person.py
+++ person.py
@@ -13,8 +13,7 @@ class Person(object):
         self.p2 = np.round(coordinates[2:]).astype(np.int)
         self.h = self.p2[1] - self.p1[1]
         self.w = self.p2[0] - self.p1[0]
-        mean_displacement = np.mean(self.p2 - self.p1, axis=0)
-        self.centroid = self.p1 + mean_displacement
+        self.centroid = self.p1 + np.mean([self.p2, self.p1], axis=0)
         self.color = color
 
     def draw_bounding_box_on_img(self, img):
