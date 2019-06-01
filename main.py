6e06c7c376adc11473c6a7bb3dc693518c7b28bc
matteopulega <45599916+matteopulega@users.noreply.github.com>
2019-06-01 16:09:17 +0200

    update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from scratch_dami import main_dami
 from scratch_marco import main_marco
-from scratch_matteo import main_matteo, main_matteo_old_CORRETTA
+from scratch_matteo import main_matteo
 
 
 def main():
     # main_dami()
     # main_marco()
-    main_matteo_old_CORRETTA()
+    main_matteo()
 
 
 if __name__ == '__main__':
